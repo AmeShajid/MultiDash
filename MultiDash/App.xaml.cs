da9f59ebc6972755497d62b4af123b65a1499f5c
Samii Shabuse <sshabuse@gmail.com>
2025-03-01 15:07:47 -0500

    git branch

---
+++ MultiDash/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiDash;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
